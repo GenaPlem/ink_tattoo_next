c3a196a304393dbe268beb02623490833f4b9307
GenaPlem <shainkill1337@gmail.com>
2024-12-12 13:57:34 +0000

    Add fontawesome script for icons in Root Layout

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,4 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "Create Next App",
@@ -19,9 +8,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
-      </body>
+      <head>
+        <script
+          src="https://kit.fontawesome.com/03b90cca97.js"
+          crossorigin="anonymous"
+          async
+        ></script>
+      </head>
+      <body>{children}</body>
     </html>
   );
 }
