72a8e4f6e4886f203544341bd714ddb99bda1845
GenaPlem <shainkill1337@gmail.com>
2024-12-17 13:05:58 +0000

    Add Gallery component

---
+++ app/components/gallery/gallery.js
@@ -0,0 +1,46 @@
+export default function Gallery() {
+  return (
+    <section class="gallery" id="gallery">
+      <div class="container">
+        <h2>Gallery</h2>
+        <div class="gallery__images">
+          <img
+            src="assets/images/gallery_1.webp"
+            alt="Tattoo artist works example"
+            loading="lazy"
+          />
+          <img
+            src="assets/images/gallery_2.webp"
+            alt="Tattoo artist works example"
+            loading="lazy"
+          />
+          <img
+            src="assets/images/gallery_3.webp"
+            alt="Tattoo artist works example"
+            loading="lazy"
+          />
+          <img
+            src="assets/images/gallery_4.webp"
+            alt="Tattoo artist works example"
+            loading="lazy"
+          />
+          <img
+            src="assets/images/gallery_5.webp"
+            alt="Tattoo artist works example"
+            loading="lazy"
+          />
+          <img
+            src="assets/images/gallery_6.webp"
+            alt="Tattoo artist works example"
+            loading="lazy"
+          />
+          <img
+            src="assets/images/gallery_7.webp"
+            alt="Tattoo artist works example"
+            loading="lazy"
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
