40d868ab609c90e5770f56879bc693ce28a78e9b
GenaPlem <shainkill1337@gmail.com>
2024-12-17 14:22:31 +0000

    Fix Images styles

--- app/components/gallery/gallery.js
+++ app/components/gallery/gallery.js
@@ -7,41 +7,62 @@ export default function Gallery() {
       <div className="container">
         <h2>Gallery</h2>
         <div className={styles.gallery__images}>
-          <Image
-            src="/images/gallery_1.png"
-            alt="Tattoo artist works example"
-            fill
-          />
-          <Image
-            src="/images/gallery_2.png"
-            alt="Tattoo artist works example"
-            fill
-          />
-          <Image
-            src="/images/gallery_3.png"
-            alt="Tattoo artist works example"
-            fill
-          />
-          <Image
-            src="/images/gallery_4.png"
-            alt="Tattoo artist works example"
-            fill
-          />
-          <Image
-            src="/images/gallery_5.png"
-            alt="Tattoo artist works example"
-            fill
-          />
-          <Image
-            src="/images/gallery_6.png"
-            alt="Tattoo artist works example"
-            fill
-          />
-          <Image
-            src="/images/gallery_7.png"
-            alt="Tattoo artist works example"
-            fill
-          />
+          <div className={styles.image__container}>
+            <Image
+              src="/images/gallery_1.png"
+              alt="Tattoo artist works example"
+              fill
+              style={{ objectFit: "cover" }}
+            />
+          </div>
+          <div className={styles.image__container}>
+            <Image
+              src="/images/gallery_2.png"
+              alt="Tattoo artist works example"
+              fill
+              style={{ objectFit: "cover" }}
+            />
+          </div>
+          <div className={styles.image__container}>
+            <Image
+              src="/images/gallery_3.png"
+              alt="Tattoo artist works example"
+              fill
+              style={{ objectFit: "cover" }}
+            />
+          </div>
+          <div className={styles.image__container}>
+            <Image
+              src="/images/gallery_4.png"
+              alt="Tattoo artist works example"
+              fill
+              style={{ objectFit: "cover" }}
+            />
+          </div>
+          <div className={styles.image__container}>
+            <Image
+              src="/images/gallery_5.png"
+              alt="Tattoo artist works example"
+              fill
+              style={{ objectFit: "cover" }}
+            />
+          </div>
+          <div className={styles.image__container}>
+            <Image
+              src="/images/gallery_6.png"
+              alt="Tattoo artist works example"
+              fill
+              style={{ objectFit: "cover" }}
+            />
+          </div>
+          <div className={styles.image__container}>
+            <Image
+              src="/images/gallery_7.png"
+              alt="Tattoo artist works example"
+              fill
+              style={{ objectFit: "cover" }}
+            />
+          </div>
         </div>
       </div>
     </section>
