8919a1b21ea0e269e0462cd7c8dbbcf12a155761
GenaPlem <shainkill1337@gmail.com>
2024-12-17 14:03:01 +0000

    Add styles for Gallery component

--- app/components/gallery/gallery.js
+++ app/components/gallery/gallery.js
@@ -1,38 +1,46 @@
 import Image from "next/image";
+import styles from "./gallery.module.css";
 
 export default function Gallery() {
   return (
-    <section class="gallery" id="gallery">
-      <div class="container">
+    <section className={styles.gallery} id="gallery">
+      <div className="container">
         <h2>Gallery</h2>
-        <div class="gallery__images">
+        <div className={styles.gallery__images}>
           <Image
-            src="assets/images/gallery_1.webp"
+            src="/images/gallery_1.png"
             alt="Tattoo artist works example"
+            fill
           />
           <Image
-            src="assets/images/gallery_2.webp"
+            src="/images/gallery_2.png"
             alt="Tattoo artist works example"
+            fill
           />
           <Image
-            src="assets/images/gallery_3.webp"
+            src="/images/gallery_3.png"
             alt="Tattoo artist works example"
+            fill
           />
           <Image
-            src="assets/images/gallery_4.webp"
+            src="/images/gallery_4.png"
             alt="Tattoo artist works example"
+            fill
           />
           <Image
-            src="assets/images/gallery_5.webp"
+            src="/images/gallery_5.png"
             alt="Tattoo artist works example"
+            fill
           />
           <Image
-            src="assets/images/gallery_6.webp"
+            src="/images/gallery_6.png"
             alt="Tattoo artist works example"
+            fill
           />
           <Image
-            src="assets/images/gallery_7.webp"
+            src="/images/gallery_7.png"
             alt="Tattoo artist works example"
+            fill
           />
         </div>
       </div>
