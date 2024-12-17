136820bad38ad34fde5dbc683ab2be58f149d8e9
GenaPlem <shainkill1337@gmail.com>
2024-12-17 13:07:00 +0000

    Update images with Image element

--- app/components/gallery/gallery.js
+++ app/components/gallery/gallery.js
@@ -1,43 +1,38 @@
+import Image from "next/image";
+
 export default function Gallery() {
   return (
     <section class="gallery" id="gallery">
       <div class="container">
         <h2>Gallery</h2>
         <div class="gallery__images">
-          <img
+          <Image
             src="assets/images/gallery_1.webp"
             alt="Tattoo artist works example"
-            loading="lazy"
           />
-          <img
+          <Image
             src="assets/images/gallery_2.webp"
             alt="Tattoo artist works example"
-            loading="lazy"
           />
-          <img
+          <Image
             src="assets/images/gallery_3.webp"
             alt="Tattoo artist works example"
-            loading="lazy"
           />
-          <img
+          <Image
             src="assets/images/gallery_4.webp"
             alt="Tattoo artist works example"
-            loading="lazy"
           />
-          <img
+          <Image
             src="assets/images/gallery_5.webp"
             alt="Tattoo artist works example"
-            loading="lazy"
           />
-          <img
+          <Image
             src="assets/images/gallery_6.webp"
             alt="Tattoo artist works example"
-            loading="lazy"
           />
-          <img
+          <Image
             src="assets/images/gallery_7.webp"
             alt="Tattoo artist works example"
-            loading="lazy"
           />
         </div>
       </div>
