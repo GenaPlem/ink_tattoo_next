82eef2b873d8943b580ab86c8d89f0dc5a88880d
GenaPlem <shainkill1337@gmail.com>
2024-12-18 15:21:43 +0000

    Add Footer component

---
+++ app/components/footer/footer.js
@@ -0,0 +1,10 @@
+export default function Footer() {
+  return (
+    <footer>
+      <div class="container">
+        <p>2023</p>
+        <p>Custom Concepts Tattoo and Design, All right reserved.</p>
+      </div>
+    </footer>
+  );
+}
