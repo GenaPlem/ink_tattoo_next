8dd2391d9538a2bf14e4e17290b560b233fa02b3
GenaPlem <shainkill1337@gmail.com>
2025-02-06 12:57:24 +0000

    Update post function with sendgrid mail sending

--- app/api/sendMail/route.js
+++ app/api/sendMail/route.js
@@ -1,8 +1,37 @@
-import nodemailer from "nodemailer";
+import { NextResponse } from "next/server";
+import sgMail from "@sendgrid/mail";
+
+sgMail.setApiKey(process.env.SEND_GRID_API_KEY);
 
 export async function POST(req) {
   try {
     const { name, number, email, description } = await req.json();
+
+    if (!name || !number || !email || !description) {
+      return NextResponse.json(
+        { error: "All fields are required!" },
+        { status: 400 }
+      );
+    }
+
+    const msg = {
+      to: process.env.EMAIL_TO,
+      from: process.env.EMAIL_FROM,
+      subject: "Ink Tattoo request",
+      text: `
+        Name: ${name}
+        Phone number: ${number}
+        Email: ${email}
+        Description: ${description}
+      `,
+    };
+
+    await sgMail.send(msg);
+
+    return NextResponse.json(
+      { message: "Mail successfully sent!" },
+      { status: 200 }
+    );
   } catch (error) {
     console.error("Error sending email:", error);
 
