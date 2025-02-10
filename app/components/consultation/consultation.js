98155ef4433c0cb82845a414c100955be4f085d2
GenaPlem <shainkill1337@gmail.com>
2025-02-10 11:53:38 +0000

    Update form with form state

--- app/components/consultation/consultation.js
+++ app/components/consultation/consultation.js
@@ -1,47 +1,89 @@
+"use client";
+import { useState } from "react";
 import styles from "./consultation.module.css";
 
 export default function Consultation() {
+  const [formData, setFormData] = useState({
+    name: "",
+    number: "",
+    email: "",
+    description: "",
+  });
+
+  const [status, setStatus] = useState("");
+
   return (
     <section className={styles.consultation} id="consultation">
       <div className="container">
         <div className={styles.consultation__wrapper}>
           <h2>Consultation</h2>
-          <p>We`ll contact you shorty</p>
-          <form action="https://formdump.codeinstitute.net" method="post">
-            <label htmlFor="name"></label>
+          <p>We`ll contact you shortly</p>
+          <form>
+            <label htmlFor="name" className="sr-only">
+              Name
+            </label>
             <input
               type="text"
               name="name"
               id="name"
               placeholder="Your name"
               required
+              value={formData.name}
+              onChange={(e) =>
+                setFormData({ ...formData, name: e.target.value })
+              }
             />
-            <label htmlFor="number"></label>
+
+            <label htmlFor="number" className="sr-only">
+              Phone number
+            </label>
             <input
               type="text"
               name="number"
               id="number"
               placeholder="Phone number"
               required
+              value={formData.number}
+              onChange={(e) =>
+                setFormData({ ...formData, number: e.target.value })
+              }
             />
-            <label htmlFor="email"></label>
+
+            <label htmlFor="email" className="sr-only">
+              Email
+            </label>
             <input
               type="email"
               name="email"
               id="email"
               placeholder="E-mail"
               required
+              value={formData.email}
+              onChange={(e) =>
+                setFormData({ ...formData, email: e.target.value })
+              }
             />
-            <label htmlFor="description"></label>
+
+            <label htmlFor="description" className="sr-only">
+              Description
+            </label>
             <textarea
               name="description"
               id="description"
               cols="30"
               rows="6"
               placeholder="Tell us about the design, style, and details..."
-            ></textarea>
+              value={formData.description}
+              onChange={(e) =>
+                setFormData({ ...formData, description: e.target.value })
+              }
+            />
+
             <input type="submit" value="Send" />
           </form>
+
+          {status && <p>{status}</p>}
+
           <a href="privacy_policy.html" id={styles.privacy_policy}>
             Privacy Policy
           </a>
