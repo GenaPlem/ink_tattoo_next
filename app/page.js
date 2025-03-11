62f218e6285408a17af8ceb529d01461324c1702
GenaPlem <shainkill1337@gmail.com>
2025-03-11 13:50:41 +0000

    Remove Header and Footer from HomePage

--- app/page.js
+++ app/page.js
@@ -1,16 +1,13 @@
-import Header from "./components/header/header";
 import Hero from "./components/hero/hero";
 import About from "./components/about/about";
 import Gallery from "./components/gallery/gallery";
 import Faq from "./components/faq/faq";
 import Consultation from "./components/consultation/consultation";
 import Contact from "./components/contact/contact";
-import Footer from "./components/footer/footer";
 
 export default function HomePage() {
   return (
     <>
-      <Header />
       <main>
         <Hero />
         <About />
@@ -19,7 +16,6 @@ export default function HomePage() {
         <Consultation />
         <Contact />
       </main>
-      <Footer />
     </>
   );
 }
